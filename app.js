e640e876a190d32fb6d83cc45dc7c566ca197f81
Xuyang Jia <jiaxuyang@outlook.com>
2019-04-23 20:51:21 +0800

    asd

--- app.js
+++ app.js
@@ -3,7 +3,15 @@
  */
 const koa = require('koa'),
     router = require('koa-router')(),
-    app = new koa()
+    mongoose = require('mongoose'),
+    app = new koa(),
+    port = process.env.PORT || 5000;
 
-app.use(router.routes())
-app.listen(3000, () => console.log('app started at 3000...'))
+router.get('/', async ctx => ctx.body = {msg: 'hello Koa'})
+mongoose.connect('mongodb+srv://jiaxuyang:jiaxuyang@koadb-rs08k.mongodb.net/test?retryWrites=true', { useNewUrlParser: true })
+.then(() => console.log('success'))
+.catch(err => console.log(err))
+
+// 配置路由
+app.use(router.routes()).use(router.allowedMethods())
+app.listen(port, () => console.log(`app started at ${port}...`))
