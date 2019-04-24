0cba27c5a6e8a8e28bb9defb61b2838562969109
Xuyang Jia <jiaxuyang@outlook.com>
2019-04-24 18:51:17 +0800

    sad

--- app.js
+++ app.js
@@ -3,15 +3,12 @@
  */
 const koa = require('koa'),
     router = require('koa-router')(),
-    mongoose = require('mongoose'),
     app = new koa(),
+    users = require('./routes/api/user')
     port = process.env.PORT || 5000;
-
-router.get('/', async ctx => ctx.body = {msg: 'hello Koa'})
-mongoose.connect('mongodb+srv://jiaxuyang:jiaxuyang@koadb-rs08k.mongodb.net/test?retryWrites=true', { useNewUrlParser: true })
-.then(() => console.log('success'))
-.catch(err => console.log(err))
-
+    
 // 配置路由
+router.get('/', async ctx => ctx.body = {msg: 'hello Koa'})
+router.use('/api/users', users)
 app.use(router.routes()).use(router.allowedMethods())
 app.listen(port, () => console.log(`app started at ${port}...`))
---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+// 实例化数据模板
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+})
+
+mongoose.connect('mongodb://localhost/douban', { useNewUrlParser: true })
+.then(() => console.log('success'))
+.catch(err => console.log(err))
+
+module.exports = User = mongoose.model('users', UserSchema)
